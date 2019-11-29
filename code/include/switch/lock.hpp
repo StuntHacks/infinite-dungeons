d9176606b9cedaa1d0022472807fda8880e75a13
Liam Minopulos <me@stunthacks.eu>
2019-11-30 00:38:48 +0100

    Add Lock class

---
+++ code/include/switch/lock.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "mutex.hpp"
+
+namespace ta {
+    class Mutex;
+
+    /**
+     * @brief Used to automatically and easily lock and unlock mutexes based on scope
+     */
+    class Lock {
+    public:
+        /**
+         * @brief Creates the lock
+         * @param t_mutex The mutex to lock
+         */
+        Lock(Mutex& t_mutex);
+
+        /**
+         * @brief Destructs the lock
+         * @note This automatically unlocks it's mutex
+         */
+        ~Lock();
+
+    private:
+        /* data */
+        Mutex m_mutex;
+    };
+} /* ta */
