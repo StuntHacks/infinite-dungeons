3753fedf4c79ecfd8c2f907dbe2bb7da5503c2f3
Liam Minopulos <me@stunthacks.eu>
2019-11-30 00:45:40 +0100

    Add common header

---
+++ code/include/common.hpp
@@ -0,0 +1,7 @@
+#include <switch.h>
+
+#include "opengl.hpp"
+#include "switch/switch.hpp"
+#include "statemachine/gamestates.hpp"
+#include "global/Application.hpp"
+#include "global/graphics/graphics.hpp"
