435f1a12f4f515fc43e04a07c5075a1965065d40
Liam Minopulos <me@stunthacks.eu>
2019-11-30 00:43:08 +0100

    Add Color class

---
+++ code/include/global/graphics/Color.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+namespace ta {
+    namespace graphics {
+        /**
+         * @brief Represents a color
+         */
+        class Color {
+        public:
+            /**
+             * @brief Creates a color using an opaque black as the default color
+             */
+            Color();
+
+            /**
+             * @brief Creates a color using a red, green and blue value
+             * @param r The red value (0.0-1.0)
+             * @param g The green value (0.0-1.0)
+             * @param b The blue value (0.0-1.0)
+             */
+            Color(float r, float g, float b);
+
+            /**
+             * @brief Creates a color using a red, green, blue and alpha value
+             * @param r The red value (0.0-1.0)
+             * @param g The green value (0.0-1.0)
+             * @param b The blue value (0.0-1.0)
+             * @param a The alpha value (0.0-1.0)
+             */
+            Color(float r, float g, float b, float a);
+
+            /**
+             * @brief Sets the red, green and blue value of the color
+             * @param r The red value (0.0-1.0)
+             * @param g The green value (0.0-1.0)
+             * @param b The blue value (0.0-1.0)
+             */
+            void setColor(float r, float g, float b);
+
+            /**
+             * @brief Sets the red, green, blue and alpha value of the color
+             * @param r The red value (0.0-1.0)
+             * @param g The green value (0.0-1.0)
+             * @param b The blue value (0.0-1.0)
+             * @param a The alpha value (0.0-1.0)
+             */
+            void setColor(float r, float g, float b, float a);
+
+            /**
+             * @brief Sets the red value of the color
+             * @param r The red value (0.0-1.0)
+             */
+            void setRed(float r);
+
+            /**
+             * @brief Returns the red value of the color
+             * @return The red value (0.0-1.0)
+             */
+            float getRed();
+
+            /**
+             * @brief Sets the green value of the color
+             * @param g The green value (0.0-1.0)
+             */
+            void setGreen(float g);
+
+            /**
+             * @brief Returns the green value of the color
+             * @return The green value (0.0-1.0)
+             */
+            float getGreen();
+
+            /**
+             * @brief Sets the blue value of the color
+             * @param b The blue value (0.0-1.0)
+             */
+            void setBlue(float b);
+
+            /**
+             * @brief Returns the blue value of the color
+             * @return The blue value (0.0-1.0)
+             */
+            float getBlue();
+
+            /**
+             * @brief Sets the alpha value of the color
+             * @param a The alpha value (0.0-1.0)
+             */
+            void setAlpha(float a);
+
+            /**
+             * @brief Returns the alpha value of the color
+             * @return The alpha value (0.0-1.0)
+             */
+            float getAlpha();
+
+        private:
+            /* data */
+            float m_red, m_green, m_blue, m_alpha;
+        };
+    } /* graphics */
+} /* ta */
---
+++ code/source/global/graphics/Color.cpp
@@ -0,0 +1,68 @@
+#include "global/graphics/Color.hpp"
+
+namespace ta {
+    namespace graphics {
+        Color::Color() :
+            m_red(0.f),
+            m_green(0.f),
+            m_blue(0.f),
+            m_alpha(1.f) { /* do nothing */ }
+
+        Color::Color(float r, float g, float b) :
+            m_red(r),
+            m_green(g),
+            m_blue(b),
+            m_alpha(1.f) { /* do nothing */ }
+
+        Color::Color(float r, float g, float b, float a) :
+            m_red(r),
+            m_green(g),
+            m_blue(b),
+            m_alpha(a) { /* do nothing */ }
+
+        void Color::setColor(float r, float g, float b) {
+            m_red = r;
+            m_green = g;
+            m_blue = b;
+        }
+
+        void Color::setColor(float r, float g, float b, float a) {
+            m_red = r;
+            m_green = g;
+            m_blue = b;
+            m_alpha = a;
+        }
+
+        void Color::setRed(float r) {
+            m_red = r;
+        }
+
+        float Color::getRed() {
+            return m_red;
+        }
+
+        void Color::setGreen(float g) {
+            m_green = g;
+        }
+
+        float Color::getGreen() {
+            return m_green;
+        }
+
+        void Color::setBlue(float b) {
+            m_blue = b;
+        }
+
+        float Color::getBlue() {
+            return m_blue;
+        }
+
+        void Color::setAlpha(float a) {
+            m_alpha = a;
+        }
+
+        float Color::getAlpha() {
+            return m_alpha;
+        }
+    } /* graphics */
+} /* ta */
