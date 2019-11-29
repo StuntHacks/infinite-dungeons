cb7c3b97be7bdef07a79ba656aa25d211e1ec74a
Liam Minopulos <me@stunthacks.eu>
2019-11-30 00:42:31 +0100

    Add Vertex and float-Vector structs

---
+++ code/include/global/graphics/Vertex.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace ta {
+    namespace graphics {
+        struct Vector2f {
+            float u = 0.0f;
+            float v = 0.0f;
+        };
+
+        struct Vector3f {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+        };
+
+        struct Vector4f {
+            float w = 0.0f;
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+        };
+
+        struct Vertex {
+            Vector3f position;
+            Vector4f color;
+            Vector2f texCoords;
+            Vector3f normals;
+        };
+
+    } /* graphics */
+} /* ta */
