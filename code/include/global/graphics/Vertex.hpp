623ee7dfce18876b4c427da4c9f5b979c3d16838
Liam Minopulos <me@stunthacks.eu>
2019-11-30 13:37:53 +0100

    Re-Arrange Vector4f members

--- code/include/global/graphics/Vertex.hpp
+++ code/include/global/graphics/Vertex.hpp
@@ -14,10 +14,10 @@ namespace ta {
         };
 
         struct Vector4f {
-            float w = 0.0f;
             float x = 0.0f;
             float y = 0.0f;
             float z = 0.0f;
+            float w = 0.0f;
         };
 
         struct Vertex {
