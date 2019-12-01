dfcc614ae6fb47f942dd6fe537faa735cf379d7f
Liam Minopulos <me@stunthacks.eu>
2019-12-01 22:39:18 +0100

    Add glm

--- code/include/opengl.hpp
+++ code/include/opengl.hpp
@@ -1,3 +1,9 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <glad/glad.h>
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
