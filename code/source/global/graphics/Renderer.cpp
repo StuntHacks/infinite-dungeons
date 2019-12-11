4bd427327a211b3df9b79be088ca1806124b16d0
Liam Minopulos <me@stunthacks.eu>
2019-12-11 19:43:10 +0100

    Clear Stencil-Buffer-Bit

--- code/source/global/graphics/Renderer.cpp
+++ code/source/global/graphics/Renderer.cpp
@@ -238,7 +238,7 @@ namespace ta {
         void Renderer::render(bool clearScreen) {
             if (m_context) {
                 if (clearScreen) {
-                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
                 }
 
                 // 3D
