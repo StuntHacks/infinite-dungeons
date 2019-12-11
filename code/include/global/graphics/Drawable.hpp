ea86c53b7240c0d7e8b551f314386e243f53d3ad
Liam Minopulos <me@stunthacks.eu>
2019-12-11 19:45:02 +0100

    Remove normalize()

--- code/include/global/graphics/Drawable.hpp
+++ code/include/global/graphics/Drawable.hpp
@@ -8,18 +8,6 @@ namespace ta {
         public:
             virtual ~Drawable() {};
             virtual void draw(ta::graphics::Renderer& renderer, bool is2d = false) = 0;
-
-        protected:
-            virtual float normalize(float position, float dimension, bool invert = false) {
-                // shift coordinate center to the positive by 50% of the dimension
-                float dimensionHalfed = dimension / 2;
-                float positionShifted = position - dimensionHalfed;
-
-                // and normalize it to [-1;1]
-                float normalized = positionShifted / dimensionHalfed;
-
-                return normalized * (invert ? 1 : 1);
-            }
         };
     } /* graphics */
 } /* ta */
