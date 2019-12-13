10583b38f19c627745100abb361a706d20877fa1
Liam Minopulos <me@stunthacks.eu>
2019-12-13 22:37:34 +0100

    Re-work TextBoxManager (with callbacks!)

--- code/include/global/menu/TextBoxManager.hpp
+++ code/include/global/menu/TextBoxManager.hpp
@@ -10,6 +10,7 @@ namespace ta {
         class TextBoxManager {
         friend class ta::Application;
         friend class ta::graphics::Renderer;
+        friend class ta::menu::TextBox;
         public:
             enum class BoxType {
                 Transparent = 0,
@@ -20,7 +21,10 @@ namespace ta {
 
             static ta::menu::TextBoxManager& getInstance();
 
-            void addTextBox(ta::menu::TextBoxManager::BoxType type, const std::wstring& text, bool autoProceed = false, int displayTime = 0, int pauseBefore = 0, int pauseAfter = 0);
+            /**
+             * @param callback See ta::menu::TextBox::display()
+             */
+            void addTextBox(ta::menu::TextBoxManager::BoxType type, const std::wstring& text, bool autoProceed = false, int displayTime = 0, int pauseBefore = 0, int pauseAfter = 0, std::function<void(int)> callback = [](int){});
 
             void pause(bool pause = true);
 
@@ -43,18 +47,22 @@ namespace ta {
             TextBoxManager();
             ~TextBoxManager();
 
+            void display();
             void update();
             void draw(ta::graphics::Renderer& renderer);
+
+            void callback(int result);
 
             struct BoxData {
                 BoxType type;
                 std::wstring text;
                 int displayTime, pauseBefore, pauseAfter;
                 bool autoProceed;
+                std::function<void(int)> callback;
             };
 
             /* data */
-            bool m_pause, m_displaying;
+            bool m_pause;
             unsigned int m_cursor;
             std::vector<BoxData> m_boxStack;
             std::vector<TextBox*> m_boxes;
