b61b1144c204aec7cb7e83fa6accbed6b02fe84b
Liam Minopulos <me@stunthacks.eu>
2019-12-18 22:44:45 +0100

    Add CircleSelect base-class

---
+++ code/include/global/graphics/drawables/menu/CircleSelect.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "global/graphics/Font.hpp"
+#include "global/graphics/drawables/Text.hpp"
+
+namespace ta {
+    namespace menu {
+        class CircleSelect: public ta::graphics::Drawable {
+        public:
+            enum State {
+                FadingIn,
+                Displaying,
+                FadingOut,
+                CleanUp,
+                Finished
+            };
+
+            struct Item {
+                std::wstring title;
+                // ta::Texture& icon;
+                std::vector<std::wstring> data;
+            };
+
+            virtual std::wstring getTitle() const;
+            virtual int getSelectedID() const;
+            virtual std::vector<ta::menu::CircleSelect::Item> getOptions() const;
+            virtual ta::menu::CircleSelect::Item getSelectedOption() const;
+
+            /**
+             * @brief Displays the Selection
+             * @param callback A callback to call after a selection is done. The single parameter is the selected choice, or -1 if nothing was selected
+             */
+            virtual void display(std::vector<ta::menu::CircleSelect::Item> options, std::function<void(int)> callback = [](int){}, const std::wstring& title = L"");
+
+            /**
+             * @brief Closes the selection
+             * @param dismiss Whether to dismiss the current selection
+             */
+            virtual void close(bool dismiss = true);
+
+            virtual void update();
+            virtual void draw(ta::graphics::Renderer& renderer, bool) = 0;
+
+            virtual ta::menu::CircleSelect::State getState();
+
+        protected:
+            CircleSelect();
+
+            /* data */
+            int m_cursor, m_result, m_frameCounter;
+            ta::menu::CircleSelect::State m_state;
+            std::wstring m_title;
+            std::vector<ta::menu::CircleSelect::Item> m_options;
+
+            std::function<void(int)> m_callback;
+
+            ta::graphics::Font m_font;
+            ta::graphics::Text m_textObject;
+        };
+    } /* menu */
+} /* ta */
---
+++ code/source/global/graphics/drawables/menu/CircleSelect.cpp
@@ -0,0 +1,116 @@
+#include "global/graphics/drawables/menu/CircleSelect.hpp"
+#include "global/settings/settings.hpp"
+#include "switch/input.hpp"
+
+namespace ta {
+    namespace menu {
+        std::wstring CircleSelect::getTitle() const {
+            return m_title;
+        }
+
+        int CircleSelect::getSelectedID() const {
+            return m_result;
+        }
+
+        std::vector<ta::menu::CircleSelect::Item> CircleSelect::getOptions() const {
+            return m_options;
+        }
+
+        ta::menu::CircleSelect::Item CircleSelect::getSelectedOption() const {
+            if (m_result > -1 && m_result < m_options.size()) {
+                return m_options[m_result];
+            } else {
+                return { L"" };
+            }
+        }
+
+        void CircleSelect::display(std::vector<ta::menu::CircleSelect::Item> options, std::function<void(int)> callback, const std::wstring& title) {
+            if (m_state == ta::menu::CircleSelect::State::Finished) {
+                m_frameCounter = 0;
+                m_cursor = 0;
+                m_title = title;
+                m_state = ta::menu::CircleSelect::State::FadingIn;
+                m_options = options;
+                m_callback = callback;
+            }
+        }
+
+        void CircleSelect::close(bool dismiss) {
+            m_frameCounter = 0;
+
+            if (dismiss) {
+                m_result = -1;
+            }
+
+            m_state = ta::menu::CircleSelect::State::FadingOut;
+        }
+
+        void CircleSelect::update() {
+            if (m_state != ta::menu::CircleSelect::State::CleanUp && m_state != ta::menu::CircleSelect::State::Finished) {
+                if (ta::Input::buttonReleased(ta::Input::ZL)) {
+                    m_state = ta::menu::CircleSelect::State::CleanUp;
+                }
+            }
+
+            // Fading in and out is up to the implementing classes
+            switch (m_state) {
+            case ta::menu::CircleSelect::State::Displaying:
+                {
+                    float stickRotation = 0.0f;
+                    float stickWeight = 0.0f;
+
+                    if (stickWeight < 0.2) {
+                        m_result = -1;
+                    } else {
+                        m_result = 0;
+
+                        // TODO: Optimize this?
+                        while (stickRotation > (360.0f / m_options.size())) {
+                            m_result++;
+                            stickRotation -= (360.0f / m_options.size());
+                        }
+                    }
+                }
+
+                break;
+            case ta::menu::CircleSelect::State::CleanUp:
+                {
+                    m_frameCounter = 0;
+                    m_cursor = 0;
+
+                    m_options.clear();
+                    m_title = L"";
+
+                    m_state = ta::menu::CircleSelect::State::Finished;
+
+                    std::function<void(int)> callback = m_callback;
+                    m_callback = [](int){};
+                    callback(m_result);
+                    m_result = -1;
+                }
+
+                return;
+            default:
+                /* do nothing */
+                break;
+            }
+
+            m_frameCounter++;
+        }
+
+        ta::menu::CircleSelect::State CircleSelect::getState() {
+            return m_state;
+        }
+
+        // protected methods
+        CircleSelect::CircleSelect() :
+        m_cursor(-1),
+        m_result(-1),
+        m_frameCounter(0),
+        m_state(ta::menu::CircleSelect::State::Finished),
+        m_title(L""),
+        m_callback([](int){}),
+        m_font(ta::settings::DefaultFontPath),
+        m_textObject(m_font) { /* do nothing */ }
+    } /* menu */
+} /* ta */
