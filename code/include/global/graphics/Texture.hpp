44df7212bd331ffedd6324ed0793bd0f2ddd693f
Liam Minopulos <me@stunthacks.eu>
2019-12-28 23:58:31 +0100

    Add width and height

--- code/include/global/graphics/Texture.hpp
+++ code/include/global/graphics/Texture.hpp
@@ -13,10 +13,12 @@ namespace ta {
             bool loadFromFile(const std::string& filepath, bool smoothTexture = true, GLenum wrapS = GL_REPEAT, GLenum wrapT = GL_REPEAT);
 
             unsigned int getTexture();
+            unsigned int getWidth();
+            unsigned int getHeight();
 
         private:
             /* data */
-            unsigned int m_texture;
+            unsigned int m_texture, m_width, m_height;
         };
     } /* graphics */
 } /* ta */
