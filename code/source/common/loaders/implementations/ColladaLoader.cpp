c02daf33ff9877d3fa8b2498fa577f69214bd1e7
Liam Minopulos <me@stunthacks.eu>
2020-01-18 01:29:47 +0100

    Add ColladaLoader barebones

---
+++ code/source/common/loaders/implementations/ColladaLoader.cpp
@@ -0,0 +1,10 @@
+#include "common/loaders/implementations/ColladaLoader.hpp"
+
+namespace id {
+    namespace loaders {
+        bool ColladaLoader::loadFromMemory(const std::string& buffer) {
+
+            return true;
+        }
+    } /* loaders */
+} /* id */
