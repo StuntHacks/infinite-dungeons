322a48802e3a0be31b1e48b674452797a14e8e4d
Liam Minopulos <me@stunthacks.eu>
2020-01-18 01:28:46 +0100

    Use TextureLoader

--- code/source/common/graphics/Texture.cpp
+++ code/source/common/graphics/Texture.cpp
@@ -1,6 +1,4 @@
 #include "common/graphics/Texture.hpp"
-#define STB_IMAGE_IMPLEMENTATION
-#include "extern/stb_image.h"
 #include "common/Console.hpp"
 
 namespace id {
@@ -26,43 +24,43 @@ namespace id {
         }
 
         bool Texture::loadFromFile(const std::string& filepath, bool smoothTexture, GLenum wrapS, GLenum wrapT) {
-            if (m_texture != 0) {
-                return false;
-            }
-
-            glGenTextures(1, &m_texture);
-
-            if (!m_texture) {
-                return false;
-            }
-
-            glBindTexture(GL_TEXTURE_2D, m_texture);
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, smoothTexture ? GL_LINEAR : GL_NEAREST);
-
-            int width, height, nrChannels;
-            unsigned char* data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
-
-            if (data) {
-                glTexImage2D(GL_TEXTURE_2D, 0, nrChannels < 4 ? GL_RGB : GL_RGBA, width, height, 0, nrChannels < 4 ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, data);
-                glGenerateMipmap(GL_TEXTURE_2D);
-                m_width = width;
-                m_height = height;
-
-                id::Console::success("Successfully loaded texture \"" + filepath + "\"!", "Texture.cpp:36");
-            } else {
-                id::Console::error("Failed to load texture \"" + filepath + "\"!", "Texture.cpp:36");
-                stbi_image_free(data);
-                return false;
-            }
-
-            stbi_image_free(data);
-            glBindTexture(GL_TEXTURE_2D, 0);
-
-            return true;
+            // if (m_texture != 0) {
+            //     return false;
+            // }
+            //
+            // glGenTextures(1, &m_texture);
+            //
+            // if (!m_texture) {
+            //     return false;
+            // }
+            //
+            // glBindTexture(GL_TEXTURE_2D, m_texture);
+            //
+            // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
+            // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
+            // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+            // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, smoothTexture ? GL_LINEAR : GL_NEAREST);
+            //
+            // int width, height, nrChannels;
+            // unsigned char* data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
+            //
+            // if (data) {
+            //     glTexImage2D(GL_TEXTURE_2D, 0, nrChannels < 4 ? GL_RGB : GL_RGBA, width, height, 0, nrChannels < 4 ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, data);
+            //     glGenerateMipmap(GL_TEXTURE_2D);
+            //     m_width = width;
+            //     m_height = height;
+            //
+            //     id::Console::success("Successfully loaded texture \"" + filepath + "\"!", "Texture.cpp:36");
+            // } else {
+            //     id::Console::error("Failed to load texture \"" + filepath + "\"!", "Texture.cpp:36");
+            //     stbi_image_free(data);
+            //     return false;
+            // }
+            //
+            // stbi_image_free(data);
+            // glBindTexture(GL_TEXTURE_2D, 0);
+            //
+            // return true;
         }
 
         unsigned int Texture::getTexture() {
