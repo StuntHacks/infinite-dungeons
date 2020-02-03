ed7c81e01a48b4d8aeee2d3daa68171c81acb00e
Liam Minopulos <me@stunthacks.eu>
2020-02-03 16:45:55 +0100

    Split between PC and Switch

--- code/include/opengl.hpp
+++ code/include/opengl.hpp
@@ -1,6 +1,12 @@
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <glad/glad.h>
+#ifdef __SWITCH__
+    #include <EGL/egl.h>
+    #include <EGL/eglext.h>
+    #include <glad/glad.h>
+#else
+    #ifdef __PC__
+        #include <GLFW/glfw3.h>
+    #endif
+#endif
 
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
