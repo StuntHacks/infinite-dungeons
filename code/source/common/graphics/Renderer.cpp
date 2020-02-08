934ffe59dee5802b305f29e777dcf25d099a20bd
Liam Minopulos <me@stunthacks.eu>
2020-02-08 12:52:36 +0100

    Fix GLSL version

--- code/source/common/graphics/Renderer.cpp
+++ code/source/common/graphics/Renderer.cpp
@@ -379,7 +379,7 @@ namespace id {
 
                 // default shaders
                 const std::string vertexShaderSource = R"text(
-                    #version 330 core
+                    #version 430 core
                     layout (location = 0) in vec3 aPos;
                     layout (location = 1) in vec4 aColor;
                     layout (location = 2) in vec2 aTexCoords;
@@ -398,7 +398,7 @@ namespace id {
                 )text";
 
                 const std::string fragmentShaderSource = R"text(
-                    #version 330 core
+                    #version 430 core
                     in vec4 ourColor;
                     out vec4 fragColor;
 
