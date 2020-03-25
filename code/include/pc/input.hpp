e729cbdd487555e32203b922ff5d2d2861653250
StuntHacks <me@stunthacks.eu>
2020-03-25 17:45:24 +0100

    Clean up

--- code/include/pc/input.hpp
+++ code/include/pc/input.hpp
@@ -5,19 +5,18 @@
  */
 #pragma once
 
-#include "common/graphics/Vertex.hpp"
-
 namespace id {
     class Application;
+    class Renderer;
 
     /**
      * @brief Contains everything specific to PC-builds
      */
     namespace pc {
         /**
-         * @brief Represents the different buttons
+         * @brief Represents different keys
          */
-        enum Button {
+        enum Key {
             Unknown = -1,     ///< Unknown key
             None,             ///< No key (never really gets triggered, meant for internal logic)
             Space = 32,       ///< Space-key
@@ -114,33 +113,44 @@ namespace id {
             F23,              ///< F23-key
             F24,              ///< F24-key
             F25,              ///< F25-key
-            KeyPad0 = 320, ///< 0 on keypad
-            KeyPad1, ///< 1 on keypad
-            KeyPad2, ///< 2 on keypad
-            KeyPad3, ///< 3 on keypad
-            KeyPad4, ///< 4 on keypad
-            KeyPad5, ///< 5 on keypad
-            KeyPad6, ///< 6 on keypad
-            KeyPad7, ///< 7 on keypad
-            KeyPad8, ///< 8 on keypad
-            KeyPad9, ///< 9 on keypad
-            KeyPadDecimal, ///< Decimal-point (.) on keypad
-            KeyPadDivide, ///< Divide (:) on keypad
-            KeyPadMultiply, ///< Multiply (*) on keypad
-            KeyPadSubstract, ///< Substract (-) on keypad
-            KeyPadAdd, ///< Add (+) on keypad
-            KeyPadEnter, ///< Enter on keypad
-            KeyPadEqual, ///< Equal (=) on keypad
-            LeftShift = 340, ///< Left Shift-key
-            LeftControl, ///< Left Ctrl-key
-            LeftAlt, ///< Left Alt-key
-            LeftSuper, ///< Left Super (Windows) key
-            RightShift, ///< Right Shift-key
-            RightControl, ///< Right Ctrl-key
-            RightAlt, ///< Right Alt-key
-            RightSuper, ///< Right Super (Windows) key
-            Menu,       ///< Menu key
-            Last = Menu, ///< ID of last element in this enum
+            KeyPad0 = 320,    ///< 0 on keypad
+            KeyPad1,          ///< 1 on keypad
+            KeyPad2,          ///< 2 on keypad
+            KeyPad3,          ///< 3 on keypad
+            KeyPad4,          ///< 4 on keypad
+            KeyPad5,          ///< 5 on keypad
+            KeyPad6,          ///< 6 on keypad
+            KeyPad7,          ///< 7 on keypad
+            KeyPad8,          ///< 8 on keypad
+            KeyPad9,          ///< 9 on keypad
+            KeyPadDecimal,    ///< Decimal-point (.) on keypad
+            KeyPadDivide,     ///< Divide (:) on keypad
+            KeyPadMultiply,   ///< Multiply (*) on keypad
+            KeyPadSubstract,  ///< Substract (-) on keypad
+            KeyPadAdd,        ///< Add (+) on keypad
+            KeyPadEnter,      ///< Enter on keypad
+            KeyPadEqual,      ///< Equal (=) on keypad
+            LeftShift = 340,  ///< Left Shift-key
+            LeftControl,      ///< Left Ctrl-key
+            LeftAlt,          ///< Left Alt-key
+            LeftSuper,        ///< Left Super (Windows) key
+            RightShift,       ///< Right Shift-key
+            RightControl,     ///< Right Ctrl-key
+            RightAlt,         ///< Right Alt-key
+            RightSuper,       ///< Right Super (Windows) key
+            Menu,             ///< Menu key
+            Last = Menu,      ///< ID of last element in this enum
+        };
+
+        /**
+         * @brief Represents the different players
+         */
+        enum class Player: int {
+            P1 = 0,   // Player 1
+            P2,       // Player 2
+            P3,       // Player 3
+            P4,       // Player 4
+            All       // All players
         };
 
         /**
@@ -148,144 +158,8 @@ namespace id {
          */
         class Input {
         friend class id::Application;
-        public:
-            /**
-             * @brief Represents the different buttons
-             */
-            enum Button {
-                None = -1, ///< -key
-            };
-
-            enum class Joystick: int {
-                // Left = JOYSTICK_LEFT,
-                // Right = JOYSTICK_RIGHT
-            };
-
-            /**
-             * @brief Represents the different players
-             */
-            enum class Player: int {
-                P1 = 0,   // Player 1
-                P2,       // Player 2
-                P3,       // Player 3
-                P4,       // Player 4
-                P5,       // Player 5
-                P6,       // Player 6
-                P7,       // Player 7
-                P8,       // Player 8
-                Handheld, // Handheld
-                All       // All players
-            };
-
-            /**
-             * @brief Contains all the input data for the current frame
-             */
-            struct InputData {
-                /**
-                 * @brief Contains input data on a per-player basis
-                 */
-                struct PlayerInput {
-                    /**
-                     * @brief Contains all the information about this player's controller
-                     */
-                    struct ControllerData {
-                        bool connected; ///< Whether a controller is connected on this slot
-                    };
-
-                    int pressedButtons,  ///< Buttons that have been pressed by this player this frame
-                        downButtons,     ///< Buttons that are pressed by this player this frame, regardless whether they were pressed in the last frame or not
-                        releasedButtons; ///< Buttons that have been released by this player this frame
-                    ControllerData controllerData; ///< Controller data
-                    id::graphics::Vector2i joystickCartesian[2];
-                    id::graphics::Vector2f joystickPolar[2];
-                };
-
-                int pressedButtons,  ///< Buttons that have been pressed by any player this frame
-                    downButtons,     ///< Buttons that are pressed by any player this frame, regardless whether they were pressed in the last frame or not
-                    releasedButtons; ///< Buttons that have been released by any player this frame
-                PlayerInput playerData[(int) Player::All]; ///< Input data for every player
-            };
-
-            /**
-             * @brief Gets the buttons down in bits
-             * @param  player The player to read the buttons from
-             * @return        Button values in bits
-             */
-            static inline unsigned int buttonsDown(Player player = Player::All) {
-                return (player == Player::All ? m_inputData.downButtons : m_inputData.playerData[(int) player].downButtons);
-            }
-
-            /**
-             * @brief Gets the buttons pressed in bits
-             * @param  player The player to read the buttons from
-             * @return        Button values in bits
-             */
-            static inline unsigned int buttonsPressed(Player player = Player::All) {
-                return (player == Player::All ? m_inputData.pressedButtons : m_inputData.playerData[(int) player].pressedButtons);
-            }
-
-            /**
-             * @brief Gets the buttons released in bits
-             * @param  player The player to read the buttons from
-             * @return        Button values in bits
-             */
-            static inline unsigned int buttonsReleased(Player player = Player::All) {
-                return (player == Player::All ? m_inputData.releasedButtons : m_inputData.playerData[(int) player].releasedButtons);
-            }
-
-            /**
-             * Returns whether the given button is pressed this frame but wasn't pressed in the last frame
-             * @param  buttons The button
-             * @param  player  The player to read the buttons from
-             * @return         Whether the button is pressed or not
-             */
-            static inline bool buttonPressed(Button buttons, Player player = Player::All) {
-                return (buttonsPressed(player) & buttons);
-            }
-
-            /**
-             * @brief Returns whether the given button is pressed this frame regardless whether it was pressed in the last frame
-             * @param  buttons The button
-             * @param  player  The player to read the buttons from
-             * @return         Whether the button is pressed or not
-             */
-            static inline bool buttonDown(Button buttons, Player player = Player::All) {
-                return (buttonsDown(player) & buttons);
-            }
-
-            /**
-             * @brief Returns whether the given button was released (i.e. isn't pressed this frame but was in the last frame)
-             * @param  buttons The button
-             * @param  player  The player to read the buttons from
-             * @return         Whether the button was released or not
-             */
-            static inline bool buttonReleased(Button buttons, Player player = Player::All) {
-                return (buttonsReleased(player) & buttons);
-            }
-
-            /**
-             * @brief Returns the position of the given stick in cartesian coordinates
-             * @param  stick  The stick to query
-             * @param  player The player to query from
-             * @return        A vector consisting of the x- and y-position (both between [-32767; 32768])
-             */
-            static inline id::graphics::Vector2i getJoystickCartesian(id::pc::Input::Joystick stick, Player player) {
-                return m_inputData.playerData[(int) player].joystickCartesian[static_cast<int>(stick)];
-            }
-
-            /**
-             * @brief Returns the position of the given stick in polar coordinates
-             * @param  stick  The stick to query
-             * @param  player The player to query from
-             * @return        A vector consisting of the angle (in degrees) and the length (between [-1.0; 1.0])
-             */
-            static inline id::graphics::Vector2f getJoystickPolar(id::pc::Input::Joystick stick, Player player) {
-                return m_inputData.playerData[(int) player].joystickPolar[static_cast<int>(stick)];
-            }
-
-        private:
+        protected:
             static void scanInput();
-            static InputData m_inputData;
         };
     } /* pc */
 } /* id */
--- code/source/pc/input.cpp
+++ code/source/pc/input.cpp
@@ -1,104 +1,10 @@
-#include <cmath>
 #include "pc/input.hpp"
 #include "opengl.hpp"
 
-#define DPAD_TO_LETTER(mask, direction, letter) \
-    if (mask & direction) {          \
-        mask &= ~direction;          \
-        mask &= letter;              \
-    }
-
 namespace id {
     namespace pc {
-        Input::InputData Input::m_inputData;
-
         void Input::scanInput() {
-            // hidScanInput();
-            //
-            // JoystickPosition posLeft, posRight;
-            // u32 keysDown, keysHeld, keysUp;
-            //
-            // m_inputData.pressedButtons  = 0;
-            // m_inputData.downButtons     = 0;
-            // m_inputData.releasedButtons = 0;
-            //
-            // for(short int i = 0; i < (short int) Player::All; ++i) {
-            //     if (isPlayerConnected((Player) i)) {
-            //         keysDown = hidKeysDown((HidControllerID) i);
-            //         keysHeld = hidKeysHeld((HidControllerID) i);
-            //         keysUp   = hidKeysUp((HidControllerID) i);
-            //
-            //         hidJoystickRead(&posLeft, (HidControllerID) i, JOYSTICK_LEFT);
-            //         hidJoystickRead(&posRight, (HidControllerID) i, JOYSTICK_RIGHT);
-            //
-            //         m_inputData.playerData[i].joystickCartesian[static_cast<int>(Joystick::Left)] = { posLeft.dx, posLeft.dy };
-            //         m_inputData.playerData[i].joystickCartesian[static_cast<int>(Joystick::Right)] = { posRight.dx, posRight.dy };
-            //
-            //         // float angleLeft = glm::degrees(atan(abs(posLeft.dy) / abs(posLeft.dx)));
-            //         // float angleRight = glm::degrees(atan(abs(posRight.dy) / abs(posRight.dx)));
-            //         float angleLeft, angleRight;
-            //
-            //         if (posLeft.dx == 0 && posLeft.dy == 0) {
-            //             angleLeft = 0.0f;
-            //         } else {
-            //             angleLeft = 180.0f - (glm::degrees(atan2(0.0f - posLeft.dy, 0.0f - posLeft.dx)) * -1.0f);
-            //             angleRight = 180.0f - (glm::degrees(atan2(0.0f - posRight.dy, 0.0f - posRight.dx)) * -1.0f);
-            //         }
-            //
-            //         if (posRight.dx == 0 && posRight.dy == 0) {
-            //             angleRight = 0.0f;
-            //         } else {
-            //             angleRight = 180.0f - (glm::degrees(atan2(0.0f - posRight.dy, 0.0f - posRight.dx)) * -1.0f);
-            //         }
-            //
-            //         m_inputData.playerData[i].joystickPolar[static_cast<int>(Joystick::Left)] = { angleLeft, static_cast<float>(sqrt(pow(abs(posLeft.dx), 2) + pow(abs(posLeft.dy), 2))) };
-            //         m_inputData.playerData[i].joystickPolar[static_cast<int>(Joystick::Right)] = { angleRight, static_cast<float>(sqrt(pow(abs(posRight.dx), 2) + pow(abs(posRight.dy), 2))) };
-            //
-            //         // since there are only d-pad buttons on the left Joy-Con, we need to convert those to A/B/X/Y-presses
-            //         if (hidGetControllerType((HidControllerID) i) == TYPE_JOYCON_LEFT) {
-            //             // Y
-            //             DPAD_TO_LETTER(keysDown, Button::DPadLeft, Button::Y)
-            //             DPAD_TO_LETTER(keysHeld, Button::DPadLeft, Button::Y)
-            //             DPAD_TO_LETTER(keysUp, Button::DPadLeft, Button::Y)
-            //
-            //             // A
-            //             DPAD_TO_LETTER(keysDown, Button::DPadRight, Button::A)
-            //             DPAD_TO_LETTER(keysHeld, Button::DPadRight, Button::A)
-            //             DPAD_TO_LETTER(keysUp, Button::DPadRight, Button::A)
-            //
-            //             // X
-            //             DPAD_TO_LETTER(keysDown, Button::DPadUp, Button::X)
-            //             DPAD_TO_LETTER(keysHeld, Button::DPadUp, Button::X)
-            //             DPAD_TO_LETTER(keysUp, Button::DPadUp, Button::X)
-            //
-            //             // B
-            //             DPAD_TO_LETTER(keysDown, Button::DPadDown, Button::B)
-            //             DPAD_TO_LETTER(keysHeld, Button::DPadDown, Button::B)
-            //             DPAD_TO_LETTER(keysUp, Button::DPadDown, Button::B)
-            //         }
-            //
-            //         m_inputData.playerData[i].controllerData.connected = true;
-            //     } else {
-            //         keysDown = 0;
-            //         keysHeld = 0;
-            //         keysUp   = 0;
-            //
-            //         m_inputData.playerData[i].joystickCartesian[static_cast<int>(Joystick::Left)] = { 0, 0 };
-            //         m_inputData.playerData[i].joystickCartesian[static_cast<int>(Joystick::Right)] = { 0, 0 };
-            //
-            //         m_inputData.playerData[i].joystickPolar[static_cast<int>(Joystick::Left)] = { 0.0f, 0.0f };
-            //         m_inputData.playerData[i].joystickPolar[static_cast<int>(Joystick::Right)] = { 0.0f, 0.0f };
-            //
-            //         m_inputData.playerData[i].controllerData.connected = false;
-            //     }
-            //
-            //     m_inputData.pressedButtons  |= keysDown;
-            //     m_inputData.downButtons     |= keysHeld;
-            //     m_inputData.releasedButtons |= keysUp;
-            //
-            //     m_inputData.playerData[i].pressedButtons  = keysDown;
-            //     m_inputData.playerData[i].downButtons     = keysHeld;
-            //     m_inputData.playerData[i].releasedButtons = keysUp;
+            glfwPollEvents();
         }
     } /* pc */
 } /* id */
